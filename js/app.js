0d32c3cd1bd701dc1a63682ed11ecc13a6674cdd
Anushree Bagchi <anushreebagchi4791@gmail.com>
2018-05-13 12:34:09 +0530

    Shuffle functionality added.

--- js/app.js
+++ js/app.js
@@ -24,6 +24,27 @@ function shuffle(array) {
 
     return array;
 }
+
+var shuffleCards= function(){
+    var cardArray= ["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt","fa fa-cube","fa fa-anchor","fa fa-leaf","fa fa-bicycle","fa fa-diamond","fa fa-bomb","fa fa-leaf","fa fa-bomb","fa fa-bolt","fa fa-bicycle","fa fa-paper-plane-o","fa fa-cube"];
+    shuffle(cardArray);
+    ulString='<ul class="deck">'
+    for (var i =0;i<cardArray.length;i++)
+    {
+        ulString+='<li class="card"><i class="'+cardArray[i]+'"></i></li>';
+    }
+    ulString+='</ul>';
+   
+   if ($(".container .deck").length===0)
+   {
+    $(".container").append(ulString);
+   }
+   else {
+     $(".container .deck" ).replaceWith(ulString);
+   }   
+  //  console.log(ulString);
+};
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -34,7 +55,7 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-var count=0, moveCount=0, a, list=[] , matchedGrid=[] , starCount; 
+var count=0, moveCount=0, a, list=[] , matchedGrid=[] , starCount, ulString; 
 
 var moves= function() {
                   moveCount++;            
@@ -49,7 +70,7 @@ var displayCard=function(el){
 var openCardList = function(el){
                      a=$(el).find("i").attr('class');  
                      list.push(a);
-                    // console.log(list);
+                     console.log(list);
                      return list;    
                 };
 
@@ -57,7 +78,7 @@ var matchedCard= function (el){
                     $(".open").addClass("match");  //adding class="match" to  matched grid
                     $(".match"). removeClass("open show");
                     matchedGrid.push($(el).find("i").attr('class'));
-                   // console.log("matched cards"+matchedGrid);
+                    console.log("matched cards"+matchedGrid);
                     if(matchedGrid.length===8)
                     {
                         gameComplete();
@@ -90,7 +111,9 @@ var restart=  function (){
     $(".moves").text(moveCount);
     $(".card"). removeClass("open show match");
     star(moveCount);
-
+    shuffleCards();
+    list=[];
+    $(".card").on("click",onClick);
 };
 
 var star= function (moveCount){
@@ -129,24 +152,30 @@ var star= function (moveCount){
      
 };
 
- $(".card").on("click",function(){
-    $(this).addClass("open show");
-    list=openCardList(this);
-        
-    if (list.length===2)
-    {
-        if(list[0]===list[1])
+var onClick= function(event){
+        $(event.target).addClass("open show");
+        list=openCardList(event.target);
+            
+        if (list.length===2)
         {
-            matchedCard(this);
+            if(list[0]===list[1])
+            {
+                matchedCard(event.target);
+            }
+            else{
+                redGrid();  //Incase   of wrong match            
+            }
+            list=[] ; // reset array
+            moveCount=moves(); // increment moves counter 
+            star(moveCount); 
         }
-        else{
-            redGrid();  //Incase   of wrong match            
-        }
-        list=[] ; // reset array
-        moveCount=moves(); // increment moves counter 
-        star(moveCount); 
-    }
- });
+};
+
+// prograam start
+
+shuffleCards();
+
+$(".card").on("click",onClick);
 
  // code to restart the game
  $(".restart").on("click",function(){
